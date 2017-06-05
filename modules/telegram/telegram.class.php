b8ff6893cb0be9d51c85f45e23f5428d672c837c
Sergei Jeihala <jey@tut.by>
2017-06-05 11:31:53 +0300

    Super-group ID fix

--- modules/telegram/telegram.class.php
+++ modules/telegram/telegram.class.php
@@ -630,7 +630,7 @@ class telegram extends module {
         }
     }
     function sendMessageToUser($user_id, $message, $key = NULL) {
-        $this->sendMessageTo('(USER_ID=' . (int)$user_id . ' OR NAME LIKE "' . DBSafe($user_id) .  '")', $message, $key);
+        $this->sendMessageTo('(USER_ID="' . DBSafe($user_id) . '" OR NAME LIKE "' . DBSafe($user_id) .  '")', $message, $key);
     }
     function sendMessageToAdmin($message, $key = NULL) {
         $this->sendMessageTo("ADMIN=1", $message, $key);
@@ -672,7 +672,7 @@ class telegram extends module {
         }
     }
     function sendImageToUser($user_id, $image_path, $message = '', $key = NULL) {
-        $this->sendImageTo('(USER_ID=' . (int)$user_id . ' OR NAME LIKE "' . DBSafe($user_id) .  '")', $image_path, $message, $key);
+        $this->sendImageTo('(USER_ID="' . DBSafe($user_id) . '" OR NAME LIKE "' . DBSafe($user_id) .  '")', $image_path, $message, $key);
     }
     function sendImageToAdmin($image_path, $message = '', $key = NULL) {
         $this->sendImageTo("ADMIN=1", $image_path, $message, $key);
@@ -713,7 +713,7 @@ class telegram extends module {
         }
     }
     function sendFileToUser($user_id, $file_path, $message = '', $key = NULL) {
-        $this->sendFileTo('(USER_ID=' . (int)$user_id . ' OR NAME LIKE "' . DBSafe($user_id) .  '")', $file_path, $message, $key);
+        $this->sendFileTo('(USER_ID="' . DBSafe($user_id) . '" OR NAME LIKE "' . DBSafe($user_id) .  '")', $file_path, $message, $key);
     }
     function sendFileToAdmin($file_path, $message = '', $key = NULL) {
         $this->sendFileTo("ADMIN=1", $file_path, $message, $key);
@@ -750,7 +750,7 @@ class telegram extends module {
         }
     }
     function sendStickerToUser($user_id, $sticker, $key = NULL) {
-        $this->sendStickerTo('(USER_ID=' . (int)$user_id . ' OR NAME LIKE "' . DBSafe($user_id) .  '")', $sticker, $key);
+        $this->sendStickerTo('(USER_ID="' . DBSafe($user_id) . '" OR NAME LIKE "' . DBSafe($user_id) .  '")', $sticker, $key);
     }
     function sendStickerToAdmin($sticker, $key = NULL) {
         $this->sendStickerTo("ADMIN=1", $sticker, $key);
@@ -789,7 +789,7 @@ class telegram extends module {
         }
     }
     function sendLocationToUser($user_id, $lat, $lon, $key = NULL) {
-        $this->sendLocationTo('(USER_ID=' . (int)$user_id . ' OR NAME LIKE "' . DBSafe($user_id) .  '")', $lat, $lon, $key);
+        $this->sendLocationTo('(USER_ID="' . DBSafe($user_id) . '" OR NAME LIKE "' . DBSafe($user_id) .  '")', $lat, $lon, $key);
     }
     function sendLocationToAdmin($lat, $lon, $key = NULL) {
         $this->sendLocationTo("ADMIN=1", $lat, $lon, $key);
@@ -832,7 +832,7 @@ class telegram extends module {
         }
     }
     function sendVenueToUser($user_id, $lat, $lon, $title, $address, $key = NULL) {
-        $this->sendVenueTo('(USER_ID=' . (int)$user_id . ' OR NAME LIKE "' . DBSafe($user_id) .  '")', $lat, $lon, $title, $address, $key);
+        $this->sendVenueTo('(USER_ID="' . DBSafe($user_id) . '" OR NAME LIKE "' . DBSafe($user_id) .  '")', $lat, $lon, $title, $address, $key);
     }
     function sendVenueToAdmin($lat, $lon, $title, $address, $key = NULL) {
         $this->sendVenueTo("ADMIN=1", $lat, $lon, $title, $address, $key);
@@ -874,7 +874,7 @@ class telegram extends module {
 		}
     }
 	function sendVoiceToUser($user_id, $file_path, $caption='', $key = NULL) {
-        $this->sendVoiceTo('(USER_ID=' . (int)$user_id . ' OR NAME LIKE "' . DBSafe($user_id) .  '")', $file_path, $caption, $key);
+        $this->sendVoiceTo('(USER_ID="' . DBSafe($user_id) . '" OR NAME LIKE "' . DBSafe($user_id) .  '")', $file_path, $caption, $key);
     }
     function sendVoiceToAdmin($file_path, $caption='', $key = NULL) {
         $this->sendVoiceTo("ADMIN=1", $file_path, $caption, $key);
