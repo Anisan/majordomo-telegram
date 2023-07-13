46a76c44862ffae0cea74e34ff072d729394ff22
Eraser <eraser1981@gmail.com>
2023-07-13 08:13:49 +0300

    history: statictics by users

--- modules/telegram/tlg_history.inc.php
+++ modules/telegram/tlg_history.inc.php
@@ -4,7 +4,7 @@ $filter = gr('filter');
 if ($filter)
     $where = "where DIRECTION IN (".$filter.")";
   // SEARCH RESULTS  
-  $res=SQLSelect('SELECT tlg_history.ID, tlg_history.CREATED, tlg_history.USER_ID,DIRECTION,TYPE,MESSAGE,tlg_user.NAME FROM tlg_history LEFT JOIN tlg_user ON tlg_history.USER_ID = tlg_user.USER_ID'.$where.' ORDER BY CREATED DESC, ID DESC');
+  $res=SQLSelect('SELECT tlg_history.ID, tlg_history.CREATED, tlg_history.USER_ID,DIRECTION,TYPE,MESSAGE,tlg_user.NAME FROM tlg_history LEFT JOIN tlg_user ON tlg_history.USER_ID = tlg_user.USER_ID '.$where.' ORDER BY CREATED DESC, ID DESC');
   if (isset($res[0])) {
     $out['COUNT']=count($res);
     $st = array_count_values(array_column($res, 'DIRECTION'));
@@ -13,6 +13,14 @@ if ($filter)
     $out['COUNT_OUT_ERROR']=$st["2"] ?? 0;
     $out['COUNT_OUT_RESEND']=$st["3"] ?? 0;
     $out['COUNT_OUT_SKIP']=$st["4"] ?? 0;
+    $st = array_count_values(array_column($res, 'NAME'));
+    $stat = [];
+    if (is_array($st)) {
+        foreach ($st as $key => $value) {
+            $stat[] = array('KEY' => $key, 'VALUE' => $value);
+        }
+    }
+    $out['STAT_USERS']=$stat;
     paging($res, 50, $out); // search result paging
     colorizeArray($res);
     $total=count($res);
